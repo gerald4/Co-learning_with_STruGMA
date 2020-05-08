fixing projected gradient, addiing informative plots

black_box.py
@@ -13,41 +13,44 @@ tfd = tfp.distributions
 
 class BlackBoxNN(tf.keras.Model):
 
-	def __init__(self, nb_units, nb_classes, data_dim = 2):
-		super(BlackBoxNN, self).__init__()
-		self.dense1 = tf.keras.layers.Dense(units = nb_units, activation=tf.nn.relu, input_shape= (data_dim,))
-		self.dense2 = tf.keras.layers.Dense(nb_units, activation=tf.nn.relu)
+    def __init__(self, nb_units, nb_classes, data_dim = 2):
+        super(BlackBoxNN, self).__init__()
+        self.dense1 = tf.keras.layers.Dense(units = nb_units, activation=tf.nn.relu, input_shape= (data_dim,))
+        self.dense2 = tf.keras.layers.Dense(nb_units, activation=tf.nn.relu)
 
-		self.classifier = tf.keras.layers.Dense(nb_classes, activation="softmax")
-
-
+        self.classifier = tf.keras.layers.Dense(nb_classes, activation="softmax")
 
 
-	def share_loss(self, X, sTGMA, weights = None):
-		kl = tf.keras.losses.KLDivergence()
-
-		def kl_divergence(x):
-			return kl(
-				tf.exp(
-					sTGMA.compute_log_conditional_distribution(x)
-					),
-			self.__call__(x),
-			sample_weight= weights
-			)
 
 
-		return tfp.monte_carlo.expectation(
-			f = kl_divergence,
-			samples = X,
-			log_prob = sTGMA.log_pdf,
-			   use_reparametrization= False
-		)
+    def share_loss(self, X, sTGMA, weights = None):
+        kl = tf.keras.losses.KLDivergence()
+
+        def kl_divergence(x):
+            return kl(
+                tf.exp(
+                    sTGMA.compute_log_conditional_distribution(x)
+                    ),
+            self.__call__(x),
+            sample_weight= weights
+            )
 
 
-	def __call__(self, inputs):
-		x = self.dense1(inputs)
-		x = self.dense2(x)
+        return tfp.monte_carlo.expectation(
+            f = kl_divergence,
+            samples = X,
+            log_prob = sTGMA.log_pdf,
+               use_reparametrization= False
+        )
+
+    def predict(self, X):
+        return tf.nn.softmax(self.__call__(X))
+
+    @tf.function
+    def __call__(self, inputs):
+        x = self.dense1(inputs)
+        x = self.dense2(x)
 
 
-		return self.classifier(x)
+        return self.classifier(x)
 

cotraining_sTGMA_NN.py
@@ -13,8 +13,9 @@ import numpy as np
 
 import tensorflow.compat.v2 as tf
 import tensorflow_probability as tfp
+import os
 
-
+from sklearn.metrics import accuracy_score
 
 
 from read_dataset_for_constraint import switch_dataset
@@ -23,174 +24,194 @@ from utils import plot_boundaries_hyperrect
 from sTGMA import SoftTruncatedGaussianMixtureAnalysis
 
 from black_box import BlackBoxNN
-
-np.set_printoptions(precision=5)
-tfd = tfp.distributions
-np.random.seed(111)
-tf.random.set_seed(111)
-
-
-
-print("TensorFlow version: {}".format(tf.__version__))
-print("Eager execution: {}".format(tf.executing_eagerly()))
-
-
-n_components = 2
-
-save_loss = []
-#np.random.seed(903604963)
-#np.random.seed(1595417368)
-#seed = np.random.seed(159541736)
-seed = np.random.seed(112)
-
-np.random.seed(seed)
-tf.random.set_seed(seed)
+from matplotlib import pyplot as plt
 
 
 
 
-
-@tf.function
-def train_step_sTGMA(data, labels, responsibilities, eta, samples):
-
-	model.eta.assign(eta)
-
-	with tf.GradientTape() as tape:
-		expected_loglikel = model(data, labels, responsibilities)
-		share_loss = model.share_loss(X = samples,  black_box_model = black_box )
-		loss = expected_loglikel
-	gradients = tape.gradient(loss, model.trainable_variables)
-	optimizer_sTGMA.apply_gradients(zip(gradients, model.trainable_variables))
-	#Projected gradients
-	val = tf.where(tf.less(model.lower + model.theta, model.upper),
-				 model.lower, (0.5)*(model.upper + model.lower - model.theta))
-
-	model.lower.assign(val)
-	val = tf.where(tf.less(model.lower + model.theta, model.upper),
-				 model.upper, (0.5)*(model.upper + model.lower + model.theta))
-
-	model.upper.assign(val)
-	return expected_loglikel, share_loss, responsibilities, gradients
-
-
-@tf.function
-def train_step_black_box(data, labels_one_hot, samples, weights = None):
-	cross_ent = tf.keras.losses.CategoricalCrossentropy()
-	with tf.GradientTape() as tape:
-
-		share_loss = black_box.share_loss(X = samples,  sTGMA = model , weights = weights)
-		cross_entropy = cross_ent(labels_one_hot, black_box(data))
-
-		loss = cross_entropy + share_loss
-	gradients = tape.gradient(loss , black_box.trainable_variables)
-
-	optimizer_black_box.apply_gradients(zip(gradients, black_box.trainable_variables))
-
-	return cross_entropy, share_loss, gradients
 
 
 
 
 if __name__== "__main__":
-	dataset_name = "data1"
-	type_eta = "eta_constant"
-	if_pca = False
 
-	X_train, y_train, X_val, y_val, X_test, y_test, y_train_onehot, y_val_onehot, y_test_onehot, scaler, color_map = \
-	    switch_dataset(dataset_name)(if_PCA = if_pca)
-	model = SoftTruncatedGaussianMixtureAnalysis(n_components = n_components, data_dim = X_train.shape[1],
-												 n_classes = len(np.unique(y_train))
+    @tf.function
+    def train_step_sTGMA(data, labels, responsibilities, eta, samples):
 
-												 )
+        model.eta.assign(eta)
 
+        with tf.GradientTape() as tape:
+            expected_loglikel = model(data, labels, responsibilities)
+            share_loss = model.share_loss(X = samples,  black_box_model = black_box )
+            loss = expected_loglikel
+        gradients = tape.gradient(loss, model.trainable_variables)
+        optimizer_sTGMA.apply_gradients(zip(gradients, model.trainable_variables))
+        #Projected gradients
+        val = tf.where(tf.less(model.lower, model.upper),
+                     model.lower, (0.5)*(model.upper + model.lower - model.theta))
 
-	model.gmm_initialisation(X_train, y_train.astype(np.int32))
+        val1 = tf.where(tf.less(model.lower, model.upper),
+                     model.upper, (0.5)*(model.upper + model.lower + model.theta))
 
-	black_box = BlackBoxNN(nb_units = 128, nb_classes = len(np.unique(y_train)))
-	tf.print(black_box.trainable_variables)
+        model.lower.assign(val)
 
-	optimizer_sTGMA = tf.optimizers.Adam(lr = 0.001)
-
-	optimizer_black_box = tf.optimizers.Adam(lr = 0.001)
-
-
-	exp_log_lik_loss = {}
-	eta = 40
-	tol = 0.002
-	loss2 = 100.0
-	save_loss1 = []
-	save_loss2 = []
-	save_share_loss1 = []
-	save_share_loss2 = []
-	diff = []
-	directory = f"images_cotraining/datasets/{dataset_name}/components_{n_components}/boundaries_importance"
+        model.upper.assign(val1)
+        return expected_loglikel, share_loss, responsibilities, gradients
 
 
-	# directory = f"{directory}/value_{eta}"
+    @tf.function
+    def train_step_black_box(data, labels_one_hot, samples, weights = None, _lambda = 1.):
+        cross_ent = tf.keras.losses.CategoricalCrossentropy()
+        with tf.GradientTape() as tape:
 
-	#os.makedirs(directory, exist_ok = True)
-	for i in range(50):
-	    #Expectation step
-		if type_eta == "eta_variant":
-			eta = (0.5)*np.sqrt(i) + eta
+            share_loss = _lambda*black_box.share_loss(X = samples,  sTGMA = model , weights = weights)
+            cross_entropy = cross_ent(labels_one_hot, black_box(data))
 
+            loss = cross_entropy + share_loss
+        gradients = tape.gradient(loss , black_box.trainable_variables)
 
+        optimizer_black_box.apply_gradients(zip(gradients, black_box.trainable_variables))
 
-		filename = f"{directory}_{i}.png"
-		responsibilities = model.compute_responsibilities(X_train, y_train.astype(np.int32))
+        return cross_entropy, share_loss, gradients
 
-
-	    #Maximization
-		print("Sampling ...")
-		#samples = model.sample_directly(nb_samples = 500).numpy()
-		samples, weights = model.sample_importance(nb_samples = 1000)
-		print(weights)
-
-		exp_log_lik_loss[i] = []
-
-		#Learning black-box model
-		for j in range(10):
-			print(f"Black-box update iteration {j}")
-			loss1, share_loss1, grad = train_step_black_box(data = X_train,
-													  labels_one_hot = y_train_onehot, samples = samples,
-													  weights = weights)
-			loss1, share_loss1 = loss1.numpy(), share_loss1.numpy()
-
-		save_loss1.append(loss1)
-		save_share_loss1.append(share_loss1)
-
-		black_box_labels = np.argmax(black_box(X_train).numpy(), axis = 1)
-
-		#Learning sTGMA
-		for j in range(100):
-
-			loss, share_loss2, resp, grad = train_step_sTGMA(data = X_train, labels = black_box_labels.astype(np.int32),
-									responsibilities = responsibilities, eta = eta, samples = samples)
-			loss, share_loss2, resp = loss.numpy(), share_loss2.numpy(), resp.numpy()
-			exp_log_lik_loss[i].append(loss)
-
-
-		plot_boundaries_hyperrect(X = X_train,
-							   y = y_train,
-							   x_axis= 0,
-							   y_axis= 1,
-							   black_box= black_box,
-							   color_map= color_map,
-							   file_name = filename,
-							   sTGMA= model,
-							   steps= 100)
-		save_loss2.append(loss)
-		save_share_loss2.append(share_loss2)
+    np.set_printoptions(precision=5)
+    tfd = tfp.distributions
+    np.random.seed(111)
+    tf.random.set_seed(111)
 
 
 
-		if np.abs(loss2 - loss)/loss2 < tol:
-			break
-		else:
-			diff.append(loss2-loss)
-			loss2 = loss
-		print(f"***** Iteration {i} *****")
-		print(f"loss1: {loss1}, shareloss1: {share_loss1}, loss2: {loss2}, shareloss2: {share_loss2}")
-	#    plot_pdfR(X_train[:,0], X_train[:,1], f"{filename}_density_{i}.png", model, color_map)
+    print("TensorFlow version: {}".format(tf.__version__))
+    print("Eager execution: {}".format(tf.executing_eagerly()))
 
 
+    n_components = 4
+
+    save_loss = []
+    #np.random.seed(903604963)
+    #np.random.seed(1595417368)
+    #seed = np.random.seed(159541736)
+    # seed = np.random.seed(112)
+
+    # np.random.seed(seed)
+    # tf.random.set_seed(seed)
+
+    dataset_name = "read_gaussian_3_quantiles"
+    type_eta = "eta_variant"
+    _lambda = 2.
+    if_pca = False
+
+    X_train, y_train, X_val, y_val, X_test, y_test, y_train_onehot, y_val_onehot, y_test_onehot, scaler, color_map = \
+        switch_dataset(dataset_name)(if_PCA = if_pca)
+    model = SoftTruncatedGaussianMixtureAnalysis(n_components = n_components, data_dim = X_train.shape[1],
+                                                 n_classes = len(np.unique(y_train))
+
+                                                 )
+
+
+    model.gmm_initialisation(X_train, y_train.astype(np.int32))
+
+    black_box = BlackBoxNN(nb_units = 128, nb_classes = len(np.unique(y_train)))
+    tf.print(black_box.trainable_variables)
+
+    optimizer_sTGMA = tf.optimizers.Adam(lr = 0.001)
+
+    optimizer_black_box = tf.optimizers.Adam(lr = 0.001)
+
+
+    exp_log_lik_loss = {}
+    eta = 40
+    tol = 0.002
+    loss2 = 100.0
+    save_loss1 = []
+    save_loss2 = []
+    save_share_loss1 = []
+    save_share_loss2 = []
+    diff = []
+    directory = f"images_cotraining/datasets/{dataset_name}/components_{n_components}"
+
+    os.makedirs(directory, exist_ok = True)
+    directory = f"{directory}/boundaries_importance"
+
+
+    # directory = f"{directory}/value_{eta}"
+
+
+    for i in range(50):
+        #Expectation step
+        if type_eta == "eta_variant":
+            eta = (0.5)*np.sqrt(i) + eta
+
+
+
+        filename = f"{directory}_{i}.png"
+        responsibilities = model.compute_responsibilities(X_train, y_train.astype(np.int32))
+
+
+        #Maximization
+        print("Sampling ...")
+        # samples = model.sample_directly(nb_samples = 500).numpy()
+        # weights = None
+        samples, weights = model.sample_importance(nb_samples = 1000)
+        weights = None
+
+        #print(weights)
+
+        exp_log_lik_loss[i] = []
+
+        #Learning black-box model
+        for j in range(10):
+            #print(f"Black-box update iteration {j}")
+            loss1, share_loss1, grad = train_step_black_box(data = X_train,
+                                                      labels_one_hot = y_train_onehot, samples = samples,
+                                                      weights = weights,
+                                                      _lambda = _lambda)
+            loss1, share_loss1 = loss1.numpy(), share_loss1.numpy()
+
+        save_loss1.append(loss1)
+        save_share_loss1.append(share_loss1)
+
+        black_box_labels = np.argmax(black_box(X_train).numpy(), axis = 1)
+
+        #Learning sTGMA
+        for j in range(100):
+
+            loss, share_loss2, resp, grad = train_step_sTGMA(data = X_train, labels = black_box_labels.astype(np.int32),
+                                    responsibilities = responsibilities, eta = eta, samples = samples)
+            loss, share_loss2, resp = loss.numpy(), share_loss2.numpy(), resp.numpy()
+            exp_log_lik_loss[i].append(loss)
+
+
+        plot_boundaries_hyperrect(X = X_train,
+                               y = y_train,
+                               x_axis= 0,
+                               y_axis= 1,
+                               black_box= black_box,
+                               color_map= color_map,
+                               file_name = filename,
+                               sTGMA= model,
+                               steps= 100)
+        save_loss2.append(loss)
+        save_share_loss2.append(share_loss2)
+
+
+
+        if np.abs(loss2 - loss)/loss2 < tol:
+            break
+        else:
+            diff.append(loss2-loss)
+            loss2 = loss
+        print(f"***** Iteration {i} *****")
+        print(f"loss1: {loss1}, shareloss1: {share_loss1}, loss2: {loss2}, shareloss2: {share_loss2}")
+    #    plot_pdfR(X_train[:,0], X_train[:,1], f"{filename}_density_{i}.png", model, color_map)
+
+
+print('----------Training----------')
+print(f"accuracy black box: {accuracy_score(y_train, np.argmax(black_box.predict(X_train).numpy(), axis = 1))}")
+print(f"accuracy sTGMA: {accuracy_score(y_train, model.predict(X_train))}")
+print(f"Fidelity: {accuracy_score(model.predict(X_train), np.argmax(black_box.predict(X_train).numpy(), axis = 1))}")
+
+print('----------Testing----------')
+print(f"accuracy black box: {accuracy_score(y_test, np.argmax(black_box.predict(X_test).numpy(), axis = 1))}")
+print(f"accuracy sTGMA: {accuracy_score(y_test, model.predict(X_test))}")
+print(f"Fidelity: {accuracy_score(model.predict(X_test), np.argmax(black_box.predict(X_test).numpy(), axis = 1))}")

utils.py
@@ -13,6 +13,7 @@ from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.cm as cm
 from scipy.special import softmax
+from sklearn.metrics import accuracy_score
 
 plt.rcParams["figure.figsize"] = (10,10)
 
@@ -116,7 +117,6 @@ def plot_boundaries_hyperrect(X, y, x_axis, y_axis, file_name, color_map, sTGMA,
 	labels_bb = np.argmax(labels_bb, axis = 1)
 
 	labels_stgma = sTGMA.predict(np.c_[xx.ravel(), yy.ravel()])
-	labels_stgma = sTGMA.predict(np.c_[xx.ravel(), yy.ravel()])
 
 	#Decision boundary sTGMA
 	plt.subplot(2, 1, 1)
@@ -133,7 +133,7 @@ def plot_boundaries_hyperrect(X, y, x_axis, y_axis, file_name, color_map, sTGMA,
 			currentAxis.add_patch(Rectangle((low[x_axis], low[y_axis]), upp[x_axis]-low[x_axis], upp[y_axis]-low[y_axis], fill=None,
 	                                    edgecolor=color_map[c], alpha=1))
 # 			plt.scatter(*mu[i,[x_axis, y_axis]], marker='^')
-	plt.title("sTGMA")
+	plt.title(f"sTGMA, accuracy_score: {accuracy_score(y, sTGMA.predict(X))}")
 
 
 	#Decision boundary ANN
@@ -141,8 +141,10 @@ def plot_boundaries_hyperrect(X, y, x_axis, y_axis, file_name, color_map, sTGMA,
 	z2 = labels_bb.reshape(xx.shape)
 	plt.contourf(xx, yy, z2, alpha=0.5)
 	plt.scatter(X1, X2, c = [color_map[y[i]] for i in range(X.shape[0])],  edgecolor='k', lw=0, cmap="Set1")
-	plt.title("ANN")
-	print("toto************************", file_name)
+	plt.title(f"ANN, accuracy_score: {accuracy_score(y, np.argmax(black_box.predict(X).numpy(), axis = 1))}")
+
+	plt.suptitle(f'fidelity: {accuracy_score(sTGMA.predict(X), np.argmax(black_box.predict(X).numpy(), axis = 1))}')
+	print("saving boundaries************************", file_name)
 	plt.savefig(file_name, dpi=150)
 	plt.clf()
 