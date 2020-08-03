eda60e40ce1b8ed2d550493ad905c12a2c0e76a2
gerald4 <nanfackg@gmail.com>
2020-08-03 14:50:31 +0200

    quite nothing

--- cotraining_sTGMA_NN.py
+++ cotraining_sTGMA_NN.py
@@ -24,27 +24,8 @@ from sTGMA import SoftTruncatedGaussianMixtureAnalysis
 
 from black_box import BlackBoxNN
 
-np.set_printoptions(precision=5)
 tfd = tfp.distributions
-np.random.seed(111)
-tf.random.set_seed(111)
-
-
-
-print("TensorFlow version: {}".format(tf.__version__))
-print("Eager execution: {}".format(tf.executing_eagerly()))
-
-
-n_components = 2
-
-save_loss = []
-#np.random.seed(903604963)
-#np.random.seed(1595417368)
-#seed = np.random.seed(159541736)
-seed = np.random.seed(112)
-
-np.random.seed(seed)
-tf.random.set_seed(seed)
+np.set_printoptions(precision=5)
 
 
 
@@ -62,23 +43,25 @@ def train_step_sTGMA(data, labels, responsibilities, eta, samples):
 	gradients = tape.gradient(loss, model.trainable_variables)
 	optimizer_sTGMA.apply_gradients(zip(gradients, model.trainable_variables))
 	#Projected gradients
-	val = tf.where(tf.less(model.lower + model.theta, model.upper),
-				 model.lower, (0.5)*(model.upper + model.lower - model.theta))
+	val = tf.where(tf.less(model.lower, model.upper),
+                 model.lower, (0.5)*(model.upper + model.lower - model.theta))
+
+	val1 = tf.where(tf.less(model.lower, model.upper),
+                 model.upper, (0.5)*(model.upper + model.lower + model.theta))
 
 	model.lower.assign(val)
-	val = tf.where(tf.less(model.lower + model.theta, model.upper),
-				 model.upper, (0.5)*(model.upper + model.lower + model.theta))
 
-	model.upper.assign(val)
+	model.upper.assign(val1)
+
 	return expected_loglikel, share_loss, responsibilities, gradients
 
 
 @tf.function
-def train_step_black_box(data, labels_one_hot, samples, weights = None):
+def train_step_black_box(data, labels_one_hot, samples, weights = None, _lambda = 1.):
 	cross_ent = tf.keras.losses.CategoricalCrossentropy()
 	with tf.GradientTape() as tape:
 
-		share_loss = black_box.share_loss(X = samples,  sTGMA = model , weights = weights)
+		share_loss = _lambda*black_box.share_loss(X = samples,  sTGMA = model , weights = weights)
 		cross_entropy = cross_ent(labels_one_hot, black_box(data))
 
 		loss = cross_entropy + share_loss
@@ -89,13 +72,66 @@ def train_step_black_box(data, labels_one_hot, samples, weights = None):
 	return cross_entropy, share_loss, gradients
 
 
+@tf.function
+def write_metrics(metrics, values, step):
+  with writer.as_default():
+
+    for met, val in zip(metrics, values):
+        tf.summary.scalar(met, val, step=step)
 
 
 if __name__== "__main__":
-	dataset_name = "data1"
-	type_eta = "eta_constant"
-	if_pca = False
 
+	np.set_printoptions(precision=5)
+	print("TensorFlow version: {}".format(tf.__version__))
+	print("Eager execution: {}".format(tf.executing_eagerly()))
+	#Setting seeds
+
+    #np.random.seed(903604963)
+	#np.random.seed(1595417368)
+	#seed = np.random.seed(159541736)
+	seed = np.random.seed(112)
+
+	np.random.seed(seed)
+	tf.random.set_seed(seed)
+
+
+
+	#Setting saving metrics and hyperparameters
+	#Hyper-parameters
+	dataset_name = "wine"
+	type_eta = "eta_variant"
+	_lambda = 2.
+	if_pca = False
+	eta = 40.
+    tol = 0.002
+    loss2 = 1000.0
+	n_components = 3
+
+
+	#Saving metrics
+	save_loss = []
+
+    save_loss1 = []
+    save_loss2 = []
+    save_share_loss1 = []
+    save_share_loss2 = []
+
+    list_train_acc_bb = []
+    list_train_acc_stgma = []
+    list_fidel_acc = []
+
+    list_fidel_test = []
+    list_test_acc_bb = []
+    list_test_acc_stgma = []
+
+    metrics = ['acc_nn_train', 'acc_nn_test',
+                'acc_stgma_train', 'acc_stgma_test',
+                'fidel_train','fidel_test', 
+                'cross_entropy','share_loss1',
+                'expected_loglikel', 'share_loss2']
+
+    #Reading dataset
 	X_train, y_train, X_val, y_val, X_test, y_test, y_train_onehot, y_val_onehot, y_test_onehot, scaler, color_map = \
 	    switch_dataset(dataset_name)(if_PCA = if_pca)
 	model = SoftTruncatedGaussianMixtureAnalysis(n_components = n_components, data_dim = X_train.shape[1],
@@ -114,18 +150,19 @@ if __name__== "__main__":
 	optimizer_black_box = tf.optimizers.Adam(lr = 0.001)
 
 
-	exp_log_lik_loss = {}
-	eta = 40
-	tol = 0.002
-	loss2 = 100.0
-	save_loss1 = []
-	save_loss2 = []
-	save_share_loss1 = []
-	save_share_loss2 = []
+
 	diff = []
-	directory = f"images_cotraining/datasets/{dataset_name}/components_{n_components}/boundaries_importance"
+    directory = f"images_cotraining/datasets/{dataset_name}/components_{n_components}"
 
+    os.makedirs(directory, exist_ok = True)
+    directory = f"{directory}/"
 
+    log_dir = f"{directory}/logs"
+    os.makedirs(log_dir, exist_ok = True)
+
+    # writer_train = tf.summary.create_file_writer(f"{log_dir}/train")
+    # writer_test = tf.summary.create_file_writer(f"{log_dir}/test")
+    writer = tf.summary.create_file_writer(f"{log_dir}")
 	# directory = f"{directory}/value_{eta}"
 
 	#os.makedirs(directory, exist_ok = True)
@@ -144,13 +181,13 @@ if __name__== "__main__":
 		print("Sampling ...")
 		#samples = model.sample_directly(nb_samples = 500).numpy()
 		samples, weights = model.sample_importance(nb_samples = 1000)
-		print(weights)
+		weights = None
 
 		exp_log_lik_loss[i] = []
 
 		#Learning black-box model
 		for j in range(10):
-			print(f"Black-box update iteration {j}")
+			#print(f"Black-box update iteration {j}")
 			loss1, share_loss1, grad = train_step_black_box(data = X_train,
 													  labels_one_hot = y_train_onehot, samples = samples,
 													  weights = weights)
@@ -165,20 +202,22 @@ if __name__== "__main__":
 		for j in range(100):
 
 			loss, share_loss2, resp, grad = train_step_sTGMA(data = X_train, labels = black_box_labels.astype(np.int32),
-									responsibilities = responsibilities, eta = eta, samples = samples)
+									responsibilities = responsibilities, eta = tf.Variable(eta, trainable = False, dtype=tf.float32), samples = samples)
 			loss, share_loss2, resp = loss.numpy(), share_loss2.numpy(), resp.numpy()
 			exp_log_lik_loss[i].append(loss)
 
 
-		plot_boundaries_hyperrect(X = X_train,
-							   y = y_train,
-							   x_axis= 0,
-							   y_axis= 1,
-							   black_box= black_box,
-							   color_map= color_map,
-							   file_name = filename,
-							   sTGMA= model,
-							   steps= 100)
+# =============================================================================
+# 		plot_boundaries_hyperrect(X = X_train,
+# 							   y = y_train,
+# 							   x_axis= 0,
+# 							   y_axis= 1,
+# 							   black_box= black_box,
+# 							   color_map= color_map,
+# 							   file_name = filename,
+# 							   sTGMA= model,
+# 							   steps= 100)
+# =============================================================================
 		save_loss2.append(loss)
 		save_share_loss2.append(share_loss2)
 
--- read_dataset_for_constraint.py
+++ read_dataset_for_constraint.py
@@ -5,6 +5,9 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import  PCA
+from sklearn.datasets import make_classification
+from sklearn.datasets import make_gaussian_quantiles
+from sklearn.datasets import load_wine
 
 from matplotlib import pyplot as plt
 
@@ -16,115 +19,215 @@ import numpy as np
 
 def preprocess(data, dataset_name, random_state = 111, if_PCA = False, use_classes=False):
 
-	#Encode labels
-	le = LabelEncoder()
-	le.fit(data['Class'].values)
-	data['Class'] = le.transform(data['Class'].values)
+    #Encode labels
+    le = LabelEncoder()
+    le.fit(data['Class'].values)
+    data['Class'] = le.transform(data['Class'].values)
 
-	#Encode for onehot
-	onehot = OneHotEncoder()
-	onehot.fit(data['Class'].values.reshape(-1,1))
+    #Encode for onehot
+    onehot = OneHotEncoder()
+    onehot.fit(data['Class'].values.reshape(-1,1))
 
-	#Split data to train and test, and then split train to train and val
-	train, test = train_test_split(data, test_size = 0.2, random_state = random_state)
+    #Split data to train and test, and then split train to train and val
+    train, test = train_test_split(data, test_size = 0.2, random_state = random_state)
 
-	train, val = train_test_split(train, test_size = 0.2, random_state = random_state)
-	train = train.values
-	val = val.values
-	test = test.values
-
-
-	#Get y_train, y_val and y_test
-	y_train = train[:,-1].astype(np.float32)
-	y_val = val[:,-1].astype(np.float32)
-	y_test = test[:,-1].astype(np.float32)
-
-	#Get the one_hot_encoding-like
-	y_train_onehot = onehot.transform(y_train.reshape(-1,1)).toarray().astype(np.float32)
-	y_val_onehot = onehot.transform(y_val.reshape(-1,1)).toarray().astype(np.float32)
-	y_test_onehot = onehot.transform(y_test.reshape(-1,1)).toarray().astype(np.float32)
-
-	#Normalising data
-	scaler = StandardScaler()
-	scaler.fit(train[:,:-1])
-
-	X_train = scaler.transform(train[:,:-1]).astype(np.float32)
-	X_val = scaler.transform(val[:,:-1]).astype(np.float32)
-	X_test = scaler.transform(test[:,:-1]).astype(np.float32)
-
-	#if PCA, apply PCA, and then return the two first axes
-	if if_PCA:
-		pca = PCA(n_components=2)
-		X_train = pca.fit(X_train).transform(X_train)[:,0:2].astype(np.float32)
-		X_test = pca.transform(X_test)[:,0:2].astype(np.float32)
-		X_val = pca.transform(X_val)[:,0:2].astype(np.float32)
+    train, val = train_test_split(train, test_size = 0.2, random_state = random_state)
+    train = train.values
+    val = val.values
+    test = test.values
 
 
-	color_map = {-1: (1, 1, 1), 0: "blue", 1: "red", 2: "green", 3: "yellow", 4: "orange", 5: "purple",
-			6: "brown", 7: "pink", 8: "gray", 9: "olive", 10: "cyan" }
 
-	return X_train, y_train, X_val, y_val, X_test, y_test, y_train_onehot, y_val_onehot, y_test_onehot, scaler, color_map
+    #Get y_train, y_val and y_test
+    y_train = train[:,-1].astype(np.float32)
+    y_val = val[:,-1].astype(np.float32)
+    y_test = test[:,-1].astype(np.float32)
+
+    #Get the one_hot_encoding-like
+    y_train_onehot = onehot.transform(y_train.reshape(-1,1)).toarray().astype(np.float32)
+    y_val_onehot = onehot.transform(y_val.reshape(-1,1)).toarray().astype(np.float32)
+    y_test_onehot = onehot.transform(y_test.reshape(-1,1)).toarray().astype(np.float32)
+
+    #Normalising data
+    scaler = StandardScaler()
+    scaler.fit(train[:,:-1])
+
+    X_train = scaler.transform(train[:,:-1]).astype(np.float32)
+    X_val = scaler.transform(val[:,:-1]).astype(np.float32)
+    X_test = scaler.transform(test[:,:-1]).astype(np.float32)
+
+    #if PCA, apply PCA, and then return the two first axes
+    if if_PCA:
+        pca = PCA(n_components=2)
+        X_train = pca.fit(X_train).transform(X_train)[:,0:2].astype(np.float32)
+        X_test = pca.transform(X_test)[:,0:2].astype(np.float32)
+        X_val = pca.transform(X_val)[:,0:2].astype(np.float32)
+
+
+    color_map = {-1: (1, 1, 1), 0: "blue", 1: "red", 2: "green", 3: "yellow", 4: "orange", 5: "purple",
+            6: "brown", 7: "pink", 8: "gray", 9: "olive", 10: "cyan" }
+
+    return X_train, y_train, X_val, y_val, X_test, y_test, y_train_onehot, y_val_onehot, y_test_onehot, scaler, color_map
 
 
 def x_train_test(X,Y,l_classes,l_dataset, use_classes=False):
-	X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.20, stratify=Y)
-	X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25, stratify=y_train)
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.20, stratify=Y)
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25, stratify=y_train)
 
-	scaler = StandardScaler().fit(X_train)
-	X_train = scaler.transform(X_train)
-	X_val = scaler.transform(X_val)
-	X_test = scaler.transform(X_test)
+    scaler = StandardScaler().fit(X_train)
+    X_train = scaler.transform(X_train)
+    X_val = scaler.transform(X_val)
+    X_test = scaler.transform(X_test)
 
 
-	C=np.unique(y_train)
-	if use_classes:
-		C=np.unique(l_classes)
-	lb=LabelBinarizer()
-	#lb.fit(C)
-	lb.fit(C)
-	y_train_ohot=lb.transform(y_train)
-	y_val_ohot=lb.transform(y_val)
-	y_test_ohot=lb.transform(y_test)
-	if len(C)==2:
-		y_train_ohot= np.hstack((1-y_train_ohot, y_train_ohot))
-		y_test_ohot= np.hstack((1-y_test_ohot, y_test_ohot))
-		y_val_ohot= np.hstack((1-y_val_ohot, y_val_ohot))
+    C=np.unique(y_train)
+    if use_classes:
+        C=np.unique(l_classes)
+    lb=LabelBinarizer()
+    #lb.fit(C)
+    lb.fit(C)
+    y_train_ohot=lb.transform(y_train)
+    y_val_ohot=lb.transform(y_val)
+    y_test_ohot=lb.transform(y_test)
+    if len(C)==2:
+        y_train_ohot= np.hstack((1-y_train_ohot, y_train_ohot))
+        y_test_ohot= np.hstack((1-y_test_ohot, y_test_ohot))
+        y_val_ohot= np.hstack((1-y_val_ohot, y_val_ohot))
 
-	N=X_train.shape[0]
-	M=X_train.shape[1]
+    N=X_train.shape[0]
+    M=X_train.shape[1]
 
-	return N, M, X_train, X_val, X_test, y_train, y_val, y_test #, y_train_ohot, y_val_ohot, y_test_ohot, l_classes,l_dataset
+    return N, M, X_train, X_val, X_test, y_train, y_val, y_test #, y_train_ohot, y_val_ohot, y_test_ohot, l_classes,l_dataset
 
 
 def read_data1(path = "data_global/data1/data1.csv", if_PCA = False):
 
-	data = pd.read_csv("data_global/data1/data1.csv")
+    data = pd.read_csv("data_global/data1/data1.csv")
 
-	return preprocess(data = data, dataset_name = "data1")
+    return preprocess(data = data, dataset_name = "data1")
+
+
+def read_two_info_one_clus(path = None, if_PCA = False):
+
+    X1, Y1 = make_classification(n_features=2, n_redundant=0, n_informative=2,
+                             n_clusters_per_class=1)
+
+    data = pd.DataFrame(np.concatenate((X1,Y1[:,None]),axis=1), columns = ['X1', 'X2', 'Class'])
+
+    return preprocess(data = data, dataset_name = "two_info_one_clus")
+
+def read_two_info_one_clus_3(path = None, if_PCA = False):
+
+    X1, Y1 = make_classification(n_features=2, n_redundant=0, n_informative=2,
+                             n_clusters_per_class=1, n_classes=3)
+
+    data = pd.DataFrame(np.concatenate((X1,Y1[:,None]),axis=1), columns = ['X1', 'X2', 'Class'])
+
+    return preprocess(data = data, dataset_name = "two_info_one_clus_3")
+
+def read_gaussian_3_quantiles(path = None, if_PCA = False):
+
+    X1, Y1 = make_gaussian_quantiles(n_features=2, n_classes=3)
+
+    data = pd.DataFrame(np.concatenate((X1,Y1[:,None]),axis=1), columns = ['X1', 'X2', 'Class'])
+
+    return preprocess(data = data, dataset_name = "two_info_one_clus_3")
+
+def read_adult(path = "data_global/adult/adult.data", if_PCA = False):
+
+    df = pd.read_csv(path, 1, ",")
+    df.columns = [
+    "Age", "WorkClass", "fnlwgt", "Education", "EducationNum",
+    "MaritalStatus", "Occupation", "Relationship", "Race", "Gender",
+    "CapitalGain", "CapitalLoss", "HoursPerWeek", "Country", "Class"
+    ]
+    df["Class"] = df["Class"].map({ "<=50K": 0, ">50K":1 })
+    df['Gender'] = df['Gender'].map({' Male':1,' Female':0}).astype(int)
+
+    df['Country'] = df['Country'].replace(' ?',np.nan)
+    df['Workclass'] = df['WorkClass'].replace(' ?',np.nan)
+    df['Occupation'] = df['Occupation'].replace(' ?',np.nan)
+    df.dropna(how='any',inplace=True)
+
+    df.loc[df['Country'] != ' United-States', 'Country'] = 'Non-US'
+    df.loc[df['Country'] == ' United-States', 'Country'] = 'US'
+    df['Country'] = df['Country'].map({'US':1,'Non-US':0}).astype(int)
+
+
+    df['MaritalStatus'] = df['MaritalStatus'].replace([' Divorced',' Married-spouse-absent',' Never-married',' Separated',' Widowed'],'Single')
+    df['MaritalStatus'] = df['MaritalStatus'].replace([' Married-AF-spouse',' Married-civ-spouse'],'Couple')
+
+
+    df['MaritalStatus'] = df['MaritalStatus'].map({'Couple':0,'Single':1})
+
+    rel_map = {' Unmarried':0,' Wife':1,' Husband':2,' Not-in-family':3,' Own-child':4,' Other-relative':5}
+
+    df['Relationship'] = df['Relationship'].map(rel_map)
+
+    race_map={' White':0,' Amer-Indian-Eskimo':1,' Asian-Pac-Islander':2,' Black':3,' Other':4}
+
+
+    df['Race']= df['Race'].map(race_map)
+    
+    def f(x):
+        if x['WorkClass'] == ' Federal-gov' or x['workclass']== ' Local-gov' or x['workclass']==' State-gov': 
+            return 'govt'
+        elif x['WorkClass'] == ' Private':
+            return 'private'
+        elif x['WorkClass'] == ' Self-emp-inc' or x['workclass'] == ' Self-emp-not-inc': 
+            return 'self_employed'
+        else: 
+            return 'without_pay'
+    
+    
+    df['employment_type']=df.apply(f, axis=1)
+
+    employment_map = {'govt':0,'private':1,'self_employed':2,'without_pay':3}
+
+    df['employment_type'] = df['employment_type'].map(employment_map)
+
+    df.drop(labels=['Workclass','Education','Occupation'],axis=1,inplace=True)
+    df.loc[(df['CapitalGain'] > 0),'CapitalGain'] = 1
+    df.loc[(df['CapitalGain'] == 0 ,'CapitalGain')]= 0
+
+    return preprocess(data = df, dataset_name = "adult", if_PCA = if_PCA)
 
 
 def read_wdbc(if_PCA =True, path="data_global/wdbc/wdbc.data"):
-	names = ["ID","Class"] + [f"Feat_{i}" for i in range(30)]
-	dataset=pd.read_csv(path,na_values='?',index_col=0,
+    names = ["ID","Class"] + [f"Feat_{i}" for i in range(30)]
+    dataset=pd.read_csv(path,na_values='?',index_col=0,
             names = names)
 
-	#X =np.asarray(dataset.values[:,0:dataset.shape[1]]-1,dtype=np.str)
-	dataset = dataset.dropna()
+    #X =np.asarray(dataset.values[:,0:dataset.shape[1]]-1,dtype=np.str)
+    dataset = dataset.dropna()
 
-	data = dataset[names[2:]+["Class"]]
+    data = dataset[names[2:]+["Class"]]
 
 
 
-	return preprocess(data = data, dataset_name = "wdbc", if_PCA = if_PCA)
+    return preprocess(data = data, dataset_name = "wdbc", if_PCA = if_PCA)
 
+def read_wine(if_PCA =False, path=None):
+    data = load_wine()
+
+    dataset = pd.DataFrame(np.concatenate((data.data,data.target[:,None]),axis=1), columns = data.feature_names + ['Class'])
+
+    
+
+    return preprocess(data = dataset, dataset_name = "wine")
 
 
 def switch_dataset(name):
-	switcher={
-		"wdbc": read_wdbc,
-		"data1": read_data1
+    switcher={
+        "wdbc": read_wdbc,
+        "data1": read_data1,
+        "two_info_one_clus": read_two_info_one_clus,
+        "two_info_one_clus_3": read_two_info_one_clus_3,
+        'read_gaussian_3_quantiles': read_gaussian_3_quantiles,
+        "adult": read_adult,
+        "wine": read_wine,
 
-	}
+    }
 
-	return switcher[name]
+    return switcher[name]
 
